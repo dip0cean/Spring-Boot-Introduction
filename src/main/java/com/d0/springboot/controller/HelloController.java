ba9f395e3d51d9d08ebe52047760e0ddb62be961
d0 <dip.0cean@daum.net>
2020-12-15 00:53:00 +0900

    Day 01 Spring Boot Introduction

---
+++ src/main/java/com/d0/springboot/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.d0.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(Model model, @RequestParam String key){
+        model.addAttribute("data",key);
+        return "hello";
+    }
+}
